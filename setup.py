f536d8cda5c34e2cedf2ca860cdcc170d02844a0
Ryan McNally <ryanfmcnally@gmail.com>
2017-11-03 00:39:17 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,21 @@
 """ Handles set up of Import.io Retry Python Package """
+import os
 from setuptools import setup
+
+def read(fname):
+    """ Function for reading files """
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
     name="Import.io Retry",
     version="0.3",
     author="Ryan McNally",
     author_email="ryan.mcnally@import.io",
+    url="https://github.com/rfmcnally/importio_retry",
     description=("A package for managing failed URLs retries on the Import.io platform."),
+    long_description=read('README.md'),
+    license="ASL",
+    platforms="any",
     packages=['importio_retry'],
     entry_points={
         'console_scripts': [
